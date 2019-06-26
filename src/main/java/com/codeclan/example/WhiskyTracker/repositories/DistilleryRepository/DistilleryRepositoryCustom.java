a616f4d3df235a51be15c7386e344cff718ff24e
Alison Williams <alison.lauriewill@gmail.com>
2019-06-26 15:06:40 +0100

    Stuff 2 electric boogaloo

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryCustom.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+
+import java.util.List;
+
 public interface DistilleryRepositoryCustom {
+
+    List<Distillery> findDistilleriesThatHaveWhiskiesAged(int age);
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,4 +1,43 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+
+    @Override
+    @Transactional
+    public List<Distillery> findDistilleriesThatHaveWhiskiesAged(int age) {
+
+        List<Distillery> result = null;
+
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Distillery.class);
+            cr.createAlias("whiskies", "whiskyAlias");
+            cr.add(Restrictions.eq("whiskyAlias.age", age));
+            result = cr.list();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+
+        }
+
+        return result;
+
+    }
 }
